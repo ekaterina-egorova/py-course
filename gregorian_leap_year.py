347190f32f63f14308de37e72fc7901b68050e7e
ekaterina-egorova <113054439+ekaterina-egorova@users.noreply.github.com>
2022-09-08 12:49:14 +0300

    V2.0
    * converted to function
    * added tests
    * waiting for input and print text result

--- gregorian_leap_year.py
+++ gregorian_leap_year.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+def is_leap(year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+    return False
 
-year = 2400
-if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-    print('Високосный год')
-else:
-    print('Обычный год')
-
+assert is_leap(2024) == True
+assert is_leap(2022) == False
+assert is_leap(2100) == False
+assert is_leap(2400) == True
+assert is_leap(2020) == True
+assert is_leap(2019) == False
+ 
+year = int(input('Введите год:'))
+print('Високосный год') if is_leap(year) else print('Обычный год')
